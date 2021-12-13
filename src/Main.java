fc58c671249378708271bf068c107ddfd47b6bc6
Paulo Henrique Oliveira Cardoso <pauloshake@gmail.com>
2021-12-13 17:07:57 -0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso POO em Java");
+		curso1.setDescricao("Descrição POO em Java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso POO em Js");
+		curso2.setDescricao("Descrição POO em Js");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria em Java");
+		mentoria.setDescricao("Descrição da mentoria Java");
+		mentoria.setData(LocalDate.now());
+		
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
 	}
 
 }
